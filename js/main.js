7a0cd571fda3ee22b951e726504f97c6e621783d
JanAksamit <76951669+JanAksamit@users.noreply.github.com>
2021-01-29 21:50:34 +0200

    socialdata

---
+++ js/main.js
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+=======
+  
+import { clock } from './components/clock.js';
+import { socialsData } from './data/socialsData.js';
+import { renderSocials } from './components/renderSocials';
+
+clock('.clock', '01-04 14:00:00');
+renderSocials('footer > .socials', socialsData);
+>>>>>>> Stashed changes
